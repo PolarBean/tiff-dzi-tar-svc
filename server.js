ee10016e681b54ef70b0c371a28b4b8fa5a93dd0
Harry Carey <38996929+PolarBean@users.noreply.github.com>
2022-10-07 17:20:02 +0200

    .

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const app = express();
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 // let users provide bucketurl as a parameter
-app.get('/bucketurl', (req, res) => {
+app.get('/bucketurl/', (req, res) => {
+    // const tarUrl = req.query.tarUrl;
     var bucket_url = req.query.bucketurl;
     console.log(bucket_url);
     iterate_over_bucket_files(bucket_url);
