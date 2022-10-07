4386c5826875fb42d722800fdb64bc9c13abcee3
Harry Carey <38996929+PolarBean@users.noreply.github.com>
2022-10-07 17:14:07 +0200

    file cleanup

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const app = express();
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8020,
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 // let users provide bucketurl as a parameter
 app.get('/bucketurl', (req, res) => {
