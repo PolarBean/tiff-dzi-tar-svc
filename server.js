0fd599ddaad57b5fee67fa1df624d85b71c2b865
Harry Carey <38996929+PolarBean@users.noreply.github.com>
2022-12-03 18:59:05 +0100

    .

--- server.js
+++ server.js
@@ -80,9 +80,7 @@ function curl_and_save(url, file) {
     // split url to get filename
     console.log(url)
     console.log(file)
-    var cmd = "curl -L " + url + "\
-    -H 'accept: application/json'\
-    -H 'Authorization: Bearer " + token + "' > " + file;
+    var cmd = "curl -L " + url + " -H 'accept: application/json' -H 'Authorization: Bearer " + token + "' > " + file;
     console.log(cmd)
     promise = exec(cmd, function (error, stdout, stderr) {
         console.log(stdout);
