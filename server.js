b6c4f2739bf5d6d568b7a5952e97616b9800d86c
Harry Carey <38996929+PolarBean@users.noreply.github.com>
2022-12-04 15:17:29 +0100

    '.'

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const path = require('path');
 const request = require('request');
 axios = require('axios');
 
-const app = require("https-localhost")()
-// const app = express();
+const app = express();
 var token = null;
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
